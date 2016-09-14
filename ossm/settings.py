f16cb120caa1e554f8cb355eb315a3af6ca022ac
Prashant Sinha <prashant+git@noop.pw>
2016-09-14 16:41:26 +0530

    Updated the settings and gitignore

--- ossm/settings.py
+++ ossm/settings.py
@@ -147,14 +147,6 @@ if not DEBUG:
     },
   ]
 
-# Debug only options
-
-if DEBUG:
-    MIDDLEWARE_CLASSES += (
-        # 'django_quicky.middleware.ForceSuperUserMiddleWare',
-    )
-
-
 SITE_ID = 1
 
 AUTH_USER_MODEL = 'people.User'
@@ -189,7 +181,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'public'), # brunch builds
 ]
