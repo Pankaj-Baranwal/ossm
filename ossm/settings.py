a4ee61b0e756c27d8cc392785949030fb88efbc4
Prashant Sinha <prashant+git@noop.pw>
2016-09-18 02:18:29 +0530

    Added whitenoise_nostatic

--- ossm/settings.py
+++ ossm/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'anymail',
     'landing',
