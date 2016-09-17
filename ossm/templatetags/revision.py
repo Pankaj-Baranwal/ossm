c951e664517317235d013251e52e9680fac94708
Prashant Sinha <prashant+git@noop.pw>
2016-09-18 04:05:27 +0530

    Added prod

--- ossm/templatetags/revision.py
+++ ossm/templatetags/revision.py
@@ -7,10 +7,13 @@ from dealer.git import git
 register = template.Library()
 
 @register.simple_tag
-def revision():
+def revision(prod=False):
+    _hrev = os.environ.get('HEROKU_SLUG_COMMIT', 'unknown')[:7]
+    rev = git.revision if git.revision else _hrev
     if settings.DEBUG or settings.STAGING:
-      rev = git.revision if git.revision else os.environ.get('HEROKU_SLUG_COMMIT', 'unknown')[:7]
       channel = 'staging' if settings.STAGING else 'dev'
       return '#%s @%s' % (rev, channel)
+    elif prod:
+      return _hrev
     else:
       return ''
