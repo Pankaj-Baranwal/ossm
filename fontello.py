3f3bdefc5646b1459303aa83345bef00643cba1f
Prashant Sinha <prashant+git@noop.pw>
2016-09-18 03:39:36 +0530

    Updated fontello config, closes #8

--- fontello.py
+++ fontello.py
@@ -1,10 +1,10 @@
 # Agora NHS
 import requests
+import shutil
 from io import BytesIO
 from zipfile import ZipFile
 
 URL = 'http://fontello.com/'
-FLLOC = 'frontend/assets/fontello-embedded.css'
 
 def fontello():
   with open('fontello.config.json', 'rb') as fl:
@@ -15,12 +15,14 @@ def fontello():
       .get('{0}{1}/get'.format(URL, session_id))
       .content)))
 
-  for cssflname in zipfl.namelist():
-    if 'fontello-embedded.css' in cssflname:
-      with open(FLLOC, 'wb') as fl:
-        fl.write(zipfl.open(cssflname).read())
-      return True
+  for fl in zipfl.namelist():
+    if 'css' in fl or 'font/' in fl:
+      loc = zipfl.extract(fl, 'frontend')
 
+  dirname = loc.split('/')[1]
+  shutil.move('frontend/%s/css' % dirname, 'frontend/assets')
+  shutil.move('frontend/%s/font' % dirname, 'frontend/assets')
+  shutil.rmtree('frontend/%s' % dirname)
 
 if __name__ == '__main__':
   fontello()
