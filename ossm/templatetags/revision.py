c6e326ee38ee39dff7a139a804239e864cc13c32
Prashant Sinha <prashant+git@noop.pw>
2016-09-18 04:00:39 +0530

    Added fallback for heroku

--- ossm/templatetags/revision.py
+++ ossm/templatetags/revision.py
@@ -1,3 +1,5 @@
+import os
+
 from django import template
 from django.conf import settings
 from dealer.git import git
@@ -7,6 +9,7 @@ register = template.Library()
 @register.simple_tag
 def revision():
     if settings.DEBUG or settings.STAGING:
+      rev = git.revision if git.revision else os.environ.get('HEROKU_SLUG_COMMIT', 'unknown')[:7]
       channel = 'staging' if settings.STAGING else 'dev'
       return '#%s @%s' % (git.revision, channel)
     else:
