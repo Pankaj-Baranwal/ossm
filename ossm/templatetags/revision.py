138206e9cd80435b5127ae99c2652e47db908ef4
Prashant Sinha <prashant+git@noop.pw>
2016-09-19 14:26:25 +0530

    Fixes syntax error

--- ossm/templatetags/revision.py
+++ ossm/templatetags/revision.py
@@ -11,7 +11,7 @@ def revision(prod=False):
     if 'HEROKU_SLUG_COMMIT' in os.environ:
       rev = os.environ['HEROKU_SLUG_COMMIT'][:7]
     else:
-      rev = git.revision if git.revision or 'unknown'
+      rev = git.revision if git.revision else 'unknown'
 
     if settings.DEBUG or settings.STAGING:
       channel = 'staging' if settings.STAGING else 'dev'
