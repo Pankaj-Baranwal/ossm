326aa1bf2789f86cf7566fb385e5b6d9c554f5da
Prashant Sinha <prashant+git@noop.pw>
2016-09-20 03:45:05 +0530

    Added initial privacy policy. Ref #12. @devkhan, please review

--- ossm/urls.py
+++ ossm/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from django.views.generic import TemplateView
 
 from . import views, admin
 
@@ -10,6 +11,9 @@ urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
     url(r'^dashboard/', include('people.urls')),
     url(r'^events/', include('events.urls')),
+
+    # Static routes:
+    url(r'^privacy-policy/$', TemplateView.as_view(template_name='privacy_policy.html'), name='privacy_policy'),
 
     # Support old routes.
     url(r'^email_read/$', views.email_read, name='email_track')
